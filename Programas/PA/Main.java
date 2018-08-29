a9a9f970f6f89e2d5c392327ab37584aab72348b
AmauriCJr <amauri_cj@hotmail.com>
2018-08-29 15:35:21 -0300

    Create Main.java

---
+++ Programas/PA/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PA 	p1 = null,
+			p2 = null;
+		
+		p1 = new PA();
+		p1.lerParametros();
+		p1.imprimirPA();
+		p1.progressao();
+	}
+
+}
