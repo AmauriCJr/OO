3d9ec87c85c2267d1ab15b562315de4eadd7234e
AmauriCJr <amauri_cj@hotmail.com>
2018-11-24 14:03:41 -0200

    Update Main.java

--- ProjetoFinal/Main.java
+++ ProjetoFinal/Main.java
@@ -1,13 +1,18 @@
-
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Questao formulario = new Questao();
 		
 		formulario.criar();
+		
+		Responder form = new Responder();
+		
+		form.responder();
 	}
 }
+
 
 
