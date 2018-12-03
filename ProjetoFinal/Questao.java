f586e613931dc650eb7f556fc8db8f4694738ea5
AmauriCJr <amauri_cj@hotmail.com>
2018-12-03 21:19:31 -0200

    Update Questao.java

--- ProjetoFinal/Questao.java
+++ ProjetoFinal/Questao.java
@@ -1,55 +1,59 @@
+package form;
+
 import java.io.IOException;
-import java.io.File;
+
 import javax.swing.JOptionPane;
 
-public class Questao {
-
-
-	public static void main(String[] args) throws IOException {}
+public class Questao extends Formulario{
+ 	
 	
-		void criar() {
-		
-		System.out.println("to aqui");
-		String nome = JOptionPane.showInputDialog("Informe o nome do formulário:");
-		
-		
-		String nomedapasta = String.format("C:\\Users\\amaur\\OneDrive\\Área de Trabalho\\Teste\\%s", nome); //cria uma variavel com o caminho do arquivo e o nome especifico do formulario
-		//é tipo o sprintf em C
-		
-		
-		File pasta = new File(nomedapasta);
-        pasta.mkdir();											//Cria uma pasta para guardar as perguntas
-		
-		for (int i = 1; i < 10000; i++) 						//Laço infinito para criar quantas perguntas o usuário desejar
-		{
-		
-			String texto = JOptionPane.showInputDialog("Informe a questão desejada:");
-		
-			if(texto.equals("fim"))									//Condicional que quebra o laço
-			{
-				break;
-			}
-		
-			String nomedoarquivo = String.format("C:\\Users\\amaur\\OneDrive\\Área de Trabalho\\Teste\\%s\\%s_Questao_%d.txt", nome, nome, i); //cria uma variavel com o caminho do arquivo e o nome especifico do formulario
-			//é tipo o sprintf em C
-		
-		
-			File file = new File(nomedoarquivo);				//Cria o arquivo com o nome escolhido
-			
-			
-			try {												//O eclipse que sugeriu colocar isso aqui pró código rodar
-				if (file.createNewFile())						//Checa se existe um arquivo com esse nome				
-					{
-						System.out.println("Arquivo Criado");
-					} else {
-						System.out.println("Já existe um arquivo com esse nome");
-					}
-			} catch (IOException e) {
-			e.printStackTrace();							//Eclipse tb sugeriu isso aqui
-			}
-		 
-			Escreve arquivo = new Escreve();					//Cria uma referência pra classe que escreve
-			arquivo.escreve(texto, nomedoarquivo);				//Escreve no arquivo
-		}	
-	}
+	protected static int numeroquestao = 1;
+	protected static int tipopergunta = 0;
+
+    public static void mostrar() throws IOException  {
+        	String opcoes[] = {
+        			"-- Selecione sua opcao --",
+        			"Questao Aberta", 
+        			"Questao Fechada", 
+        			"Retornar ao Menu Anterior"  }; 
+        
+        	Object opcao = JOptionPane.showInputDialog(null, "Escolha um tipo de questão", 
+        			"Formulario", JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+       
+  
+        	switch (opcao.toString()) {
+        	case "Questao Aberta":
+        		QuestaoAberta qa = new QuestaoAberta();
+        		qa.novaQuestaoAberta();
+        		break;
+
+	
+        	case "Questao Fechada":	
+        		QuestaoFechada qf = new QuestaoFechada();
+        		qf.novaQuestaoFechada();
+        		break;
+        		
+        	case "Retornar ao Menu Anterior":
+        		new Formulario();
+        		Formulario.mostrar();
+            	break;
+
+        
+				}
+
+        	
+        	
+    }
+      public static void main() throws IOException {
+    	  
+         while(true)
+          {
+        	 new Questao();
+        	 Questao.mostrar();
+          }
+      }
+       
+       
+       
+
 }
