0298445fe710dd36f170945fd7776004bb51275a
AmauriCJr <amauri_cj@hotmail.com>
2018-12-03 21:20:42 -0200

    Create QuestaoCurta.java

---
+++ ProjetoFinal/QuestaoCurta.java
@@ -0,0 +1,28 @@
+package form;
+
+
+
+import javax.swing.JOptionPane;
+
+public class QuestaoCurta extends QuestaoAberta {
+
+	void criar() {
+		
+            
+		
+			String texto = JOptionPane.showInputDialog("Informe a questão desejada:");
+		
+			String nomedoarquivo = String.format("C:\\Users\\amaur\\OneDrive\\Área de Trabalho\\Teste\\%s\\Questao_%d.txt",pastadestino, numeroquestao); //cria uma variavel com o caminho do arquivo e o nome especifico do formulario
+			//é tipo o sprintf em C
+		
+			numeroquestao = numeroquestao +1;
+			tipopergunta = 1;
+			
+			texto = tipopergunta + "^" + texto;
+			
+			Escreve arquivo = new Escreve();					//Cria uma referência pra classe que escreve
+			arquivo.escreve(texto, nomedoarquivo);				//Escreve no arquivo
+			
+			
+	}
+}
